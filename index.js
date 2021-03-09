1fba8ee833923e10d93f1b6af8dcc9820ca65b63
zach7815 <53745970+zach7815@users.noreply.github.com>
2021-03-09 22:25:27 +0000

    Update to CSS style Sheet and Index.JS
    CSS added media queries to make responsive at 400px width
    
    Index.JS refactored code to apply learnings from online mentoring.

--- index.js
+++ index.js
@@ -4,36 +4,25 @@ const decrease= document.querySelector(".decrease");
 const reset = document.querySelector(".reset")
 let number= 0;
 
-increase.addEventListener("click", ()=>{
-number++
- count.innerHTML=number;
- if(count.innerHTML == 0){
-    count.style.color="#354853";
-}
- if ( count.innerHTML > 0){
-    count.style.color="green";
-}
-    return count
 
-});
 
-decrease.addEventListener("click", ()=>{
-    number--
-    count.innerHTML=number;
-    if(count.innerHTML == 0){
-        count.style.color="#354853";
-    }
-    if (count.innerHTML < 0){
-        count.style.color="red";
-    }
-    return count
-    });
+const changeValue = (num) => {
+    number += num;
+   count.classList.toggle("negative", number < 0);
+   count.classList.toggle("positive", number > 0);
+   count.innerHTML=number;
+  };
+  
+  increase.addEventListener('click', () => {
+    changeValue(1);
+  });
+  
+  decrease.addEventListener('click', () => {
+    changeValue(-1);
+  });
 
     reset.addEventListener("click", () => {
         number = 0;
-       
-        count.style.color="#354853";
-        return count.innerHTML=number;
+       count.classList.remove("negative");
+       count.classList.remove("positive");
     })
-
- 
